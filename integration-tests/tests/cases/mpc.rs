61312926173fa139ea2dd017759820af424f0453
Jakob Meier <mail@jakobmeier.ch>
2025-09-11 19:57:54 +0200

    test(mpc): offline node
    Check presignatures and signatures are still generated when
    one of three nodes is offline.
    
    This is just a small incremental addition to component testing.

--- integration-tests/tests/cases/mpc.rs
+++ integration-tests/tests/cases/mpc.rs
@@ -8,7 +8,7 @@ use mpc_node::protocol::{Chain, IndexedSignRequest, ProtocolState};
 use mpc_primitives::{SignArgs, SignId};
 use std::collections::BTreeMap;
 use std::fs;
-use std::time::{Duration, Instant};
+use std::time::Duration;
 
 /// Use this toggle locally to regenerate hard-coded inputs such as key shares,
 /// triples, and presignatures.
@@ -68,9 +68,7 @@ async fn test_basic_generate_keys() {
 #[tokio::test(flavor = "multi_thread")]
 async fn test_basic_generate_triples() {
     let network = MpcFixtureBuilder::default()
-        .with_preshared_key()
-        .with_min_presignatures_stockpile(0)
-        .with_max_presignatures_stockpile(0)
+        .only_generate_triples()
         .build()
         .await;
 
@@ -111,10 +109,7 @@ async fn test_basic_generate_triples() {
 #[tokio::test(flavor = "multi_thread")]
 async fn test_basic_generate_presignature() {
     let network = MpcFixtureBuilder::default()
-        .with_preshared_key()
-        .with_preshared_triples()
-        .with_min_triples_stockpile(0)
-        .with_max_triples_stockpile(0)
+        .only_generate_presignatures()
         .build()
         .await;
 
@@ -158,12 +153,7 @@ async fn test_basic_generate_presignature() {
 #[tokio::test(flavor = "multi_thread")]
 async fn test_basic_sign() {
     let network = MpcFixtureBuilder::default()
-        .with_preshared_key()
-        .with_presignature_stockpile()
-        .with_min_triples_stockpile(0)
-        .with_max_triples_stockpile(0)
-        .with_min_presignatures_stockpile(0)
-        .with_max_presignatures_stockpile(0)
+        .only_generate_signatures()
         .build()
         .await;
 
@@ -181,36 +171,17 @@ async fn test_basic_sign() {
     network[2].sign_tx.send(request.clone()).await.unwrap();
 
     let timeout = Duration::from_secs(10);
-    let interval = Duration::from_millis(100);
-    let deadline = Instant::now() + timeout;
 
-    loop {
-        let actions = network.output.rpc_actions.lock().await;
+    let actions = tokio::time::timeout(timeout, network.wait_for_actions(1))
+        .await
+        .expect("should publish RPC action eventually");
 
-        if !actions.is_empty() {
-            assert_eq!(actions.len(), 1);
-            let action_str = actions.iter().next().unwrap();
-            assert!(
-                action_str.contains("RpcAction::Publish"),
-                "unexpected rpc action {action_str}"
-            );
-            break;
-        }
-
-        if Instant::now() >= deadline {
-            panic!("Timeout: expected 1 rpc_action but got {}", actions.len());
-        }
-
-        drop(actions);
-        tokio::time::sleep(interval).await;
-    }
-
-    // tracing::info!("printing all messages between nodes");
-    // let out = &mut std::fs::File::create("network_msg.txt").unwrap();
-    // for line in network.msg_log.lock().await.iter() {
-    //     tracing::info!("{line}");
-    //     writeln!(out, "{line}").unwrap();
-    // }
+    assert_eq!(actions.len(), 1);
+    let action_str = actions.iter().next().unwrap();
+    assert!(
+        action_str.contains("RpcAction::Publish"),
+        "unexpected rpc action {action_str}"
+    );
 }
 
 fn sign_request(seed: u8) -> IndexedSignRequest {
@@ -259,12 +230,10 @@ async fn test_presignature_timeout() {
 
     let network = MpcFixtureBuilder::default()
         // configure network ready to generate presignatures immediately
-        .with_preshared_key()
-        .with_preshared_triples()
+        .only_generate_presignatures()
+        // set exact presignature count target
         .with_min_presignatures_stockpile(5)
         .with_max_presignatures_stockpile(5)
-        .with_min_triples_stockpile(0)
-        .with_max_triples_stockpile(0)
         // apply message filter to all nodes
         .with_outgoing_message_filter(0, create_filter())
         .with_outgoing_message_filter(1, create_filter())
@@ -274,7 +243,101 @@ async fn test_presignature_timeout() {
         .build()
         .await;
 
-    tokio::time::timeout(Duration::from_secs(15), network.wait_for_presignatures(1))
+    tokio::time::timeout(Duration::from_secs(300), network.wait_for_presignatures(1))
         .await
         .expect("should have enough presignatures eventually");
+}
+
+/// Check if signatures are produced normally when one of three nodes is offline.
+#[tokio::test(flavor = "multi_thread")]
+async fn test_signature_with_offline_node() {
+    let instable_node_filter: MessageFilter = Box::new(move |(_, _)| false);
+
+    // Must be small enough to work with fixture stockpile of presignatures.
+    // Each node has at least 4 presignatures in the fixture, so 4 signatures should be possible.
+    let target_num = 4;
+    let network = MpcFixtureBuilder::default()
+        .only_generate_signatures()
+        .with_outgoing_message_filter(0, instable_node_filter)
+        .build()
+        .await;
+
+    // TODO insert signature requests
+
+    let result = tokio::time::timeout(
+        Duration::from_secs(10),
+        network.wait_for_actions(target_num),
+    )
+    .await;
+
+    match result {
+        Ok(actions) => {
+            for action_str in actions.iter() {
+                assert!(
+                    action_str.contains("RpcAction::Publish"),
+                    "unexpected rpc action {action_str}"
+                );
+            }
+        }
+        Err(_elapsed) => {
+            let actions = network.output.rpc_actions.lock().await;
+            tracing::error!("expected 4 signatures but only got {}", actions.len());
+            for action in actions.iter() {
+                tracing::error!("{action}");
+            }
+            panic!("test_signature_with_offline_node failed, see error log");
+        }
+    }
+}
+
+/// Check if presignatures are produced normally when one of three nodes is offline.
+#[tokio::test(flavor = "multi_thread")]
+async fn test_presignature_with_offline_node() {
+    let instable_node_filter: MessageFilter = Box::new(move |(_, _)| false);
+
+    // Must be small enough to work with fixture stockpile of triples.
+    // Each node has at least 8 triples in the fixture, so 4 presignatures should be possible.
+    let target_num = 4;
+
+    let network = MpcFixtureBuilder::default()
+        .only_generate_presignatures()
+        .with_outgoing_message_filter(0, instable_node_filter)
+        // set exact presignature count target
+        .with_min_presignatures_stockpile(target_num)
+        .with_max_presignatures_stockpile(target_num)
+        .build()
+        .await;
+
+    tokio::time::timeout(
+        Duration::from_secs(15),
+        network.wait_for_presignatures(target_num as usize),
+    )
+    .await
+    .expect("should have enough presignatures eventually");
+}
+
+/// Check if triples are produced normally when one of three nodes is offline.
+#[tokio::test(flavor = "multi_thread")]
+async fn test_triple_with_offline_node() {
+    let instable_node_filter: MessageFilter = Box::new(move |(_, _)| false);
+
+    // Must be small enough to not take too long. (generating triples is
+    // computationally heavy)
+    let target_num = 3;
+
+    let network = MpcFixtureBuilder::default()
+        .only_generate_triples()
+        .with_outgoing_message_filter(0, instable_node_filter)
+        // set exact triple count target
+        .with_min_triples_stockpile(target_num)
+        .with_max_triples_stockpile(target_num)
+        .build()
+        .await;
+
+    tokio::time::timeout(
+        Duration::from_secs(100),
+        network.wait_for_triples(target_num as usize),
+    )
+    .await
+    .expect("should have enough triples eventually");
 }
